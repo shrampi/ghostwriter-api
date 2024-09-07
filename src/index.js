37f358fb895171fa5f54e84bcd7f244029e60110
Carson Crow <carsoncrow@pm.me>
2024-09-07 10:49:05 -0500

    integrating typescript

--- src/index.js
+++
@@ -1,9 +0,0 @@
-const http = require("http");
-const app = require("./app");
-
-const server = http.createServer(app);
-
-const PORT = process.env.PORT || 3005;
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
